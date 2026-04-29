9c62986ff95148d1bb971e001a70f33d61f6f872
Nguyen Quang Diep <nguyenquangdiepzed1@gmail.com>
2026-04-29 22:12:33 +0700

    feat: add UserConfiguration file for configure user table with fluentapi in postgres database

---
+++ ChatApp.Repository/Configurations/UserConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using ChatApp.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.Repository.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.ToTable("users");
+
+            builder.Property(x => x.UserName)
+                .IsRequired()
+                .HasMaxLength(100);
+        }
+    }
+}
