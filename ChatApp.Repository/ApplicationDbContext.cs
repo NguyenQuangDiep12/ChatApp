40e4f69feee7e8847e326ed38da55cab636f09ad
Nguyen Quang Diep <nguyenquangdiepzed1@gmail.com>
2026-04-29 22:16:17 +0700

    feat: add ApplicationDbContext and initial database setup

---
+++ ChatApp.Repository/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using ChatApp.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.Repository
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContext) : base(dbContext) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserSetting> Settings { get; set; }
+        public DbSet<UserSystemRole> SystemRoles { get; set; }
+        public DbSet<Session> Sessions { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<RoomMember> RoomsMembers { get; set; }
+        public DbSet<RolePermission> RolePermissions { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Permission> Permissions { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<MessageRead> MessageReads { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<InviteToken> InviteTokens { get; set; }
+        public DbSet<Attachment> Attachments { get; set; }
+    }
+}
