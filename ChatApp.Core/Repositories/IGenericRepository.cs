e0b5ca47639819ae7c76fc5b979049d9cfbb2c95
Nguyen Quang Diep <nguyenquangdiepzed1@gmail.com>
2026-04-29 22:18:15 +0700

    feat: add Repository Inteface function of Entities

---
+++ ChatApp.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+
+        IQueryable<T> GetAll();
+
+        // productrepository.where(x=>x.id>5).orderBy().ToListAsync()
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+
+        Task AddAsync(T entity);
+
+        Task AddRangeAsync(IEnumerable<T> entities);
+
+        void Update(T entity); // asenkron metodu yok
+
+        void Remove(T entity);
+
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
---
+++ ChatApp.Core/Repositories/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using ChatApp.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.Core.Repositories
+{
+    public interface IUserRepository : IGenericRepository<User>
+    {
+        Task<User?> GetUserByEmailAsync(string email);
+        Task<User?> GetUserByUsernameAsync(string username);
+        Task<bool> ExistsByEmailAsync(string email);
+        Task<bool> ExistsByUsernameAsync(string username);
+        Task<User> EditUserInfoByEmail(string email);
+    }
+}
