f5f94e29b4473a00d5c839b3eb2dde4c743bf817
Diep Nguyen <nguyenquangdiepzed1@gmail.com>
2026-05-05 18:58:46 +0700

    feat: implement the interfaces of Core Layer.

---
+++ ChatApp.Repository/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using ChatApp.Core.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.Repository.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+        public UnitOfWork(ApplicationDbContext _context)
+        {
+            this._context = _context;
+        }
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
