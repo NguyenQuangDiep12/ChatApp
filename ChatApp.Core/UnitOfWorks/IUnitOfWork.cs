a2f875a38de743967c065c0ce5edd7264a0a1d01
Diep Nguyen <nguyenquangdiepzed1@gmail.com>
2026-05-05 17:38:06 +0700

    feat: define unit of work interfaces

---
+++ ChatApp.Core/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.Core.UnitOfWorks
+{
+    public interface IUnitOfWork
+    {
+        Task CommitAsync(); // Commit bat dong bo
+        void Commit(); // Commit dong bo
+    }
+}
