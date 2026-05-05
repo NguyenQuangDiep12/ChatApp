c32b378b781235acc671d1210dda76f2aa5bac70
Diep Nguyen <nguyenquangdiepzed1@gmail.com>
2026-05-05 18:48:31 +0700

    fix: rename file.

---
+++ ChatApp.Core/Repositories/IRolePermissionRepository.cs
@@ -0,0 +1,10 @@
+﻿using ChatApp.Core.Models;
+
+namespace ChatApp.Core.Repositories
+{
+    public interface IRolePermissionRepository : IGenericRepository<RolePermission>
+    {
+        Task<IEnumerable<RolePermission>> GetByRoleIdAsync(Guid roleId);
+        Task<bool> HasPermissionAsync(Guid roleId, string permissionCode);
+    }
+}
