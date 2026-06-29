bfcbe5061ec6a769ec1923b6f60176e22927c9d5
Diep Nguyen <nguyenquangdiepzed1@gmail.com>
2026-06-29 17:15:24 +0700

    feat: create interface functions for Repo.

--- ChatApp.Core/Repositories/IInviteTokenRepository.cs
+++ ChatApp.Core/Repositories/IInviteTokenRepository.cs
@@ -5,8 +5,8 @@ namespace ChatApp.Core.Repositories
     public interface IInviteTokenRepository : IGenericRepository<InviteToken>
     {
         Task<InviteToken?> GetByTokenAsync(string token);
-        Task<IEnumerable<InviteToken>> GetActiveTokensByRoomIdAsync(int roomId);
+        Task<IEnumerable<InviteToken>> GetActiveTokensByRoomIdAsync(Guid roomId);
         Task<bool> IsTokenValidAsync(string token);
-        Task IncrementUseCountAsync(int id);
+        Task IncrementUseCountAsync(Guid id);
     }
 }
